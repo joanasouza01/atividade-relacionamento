d168776f3816fb1eb5fadbbb5fcfb006e8a38758
joana de souza <joanasouzasilva.2008@gmail.coma>
2026-04-06 15:54:16 -0300

    criação class produto e fornecedor e inicialização do banco

---
+++ main.py
@@ -0,0 +1,40 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+
+
+Base = declarative_base()
+
+class Fornecedor(Base):
+    __tablename__ = "fornecedor"
+
+    #Como criar colunas
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+
+    produtos = relationship("Produto", back_populates="fornecedor")
+
+    #Função de imprimir
+    def __repr__(self):
+        return f"Fornecedor: ID = {self.id} - NOME = {self.nome}"
+    
+class Produto(Base):
+    __tablename__ = "produtos"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+    preco = Column(Float, nullable=False)
+
+    #ForeignKey é o que eu criar o vínuclo no banco
+    fornecedor_id = Column(Integer, ForeignKey("fornecedores.id"))
+
+    #Relacionamento
+    fornecedor = relationship("Fornecedor", back_populates="produtos")
+
+    def __repr__(self):
+        return f"Produto: ID = {self.id} - NOME = {self.nome} - PREÇO = {self.preco}"
+    
+engine = create_engine("sqlite:///empresa.db")
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
